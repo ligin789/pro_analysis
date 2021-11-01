e64987ddc8f51dbdc80e8b166e7aca2fa7ecac35
ligin789 <ligin789@gmail.com>
2021-11-01 14:33:16 +0530

    Document added

--- auth/index.php
+++ auth/index.php
@@ -83,13 +83,13 @@ if (isset($_SESSION["proAnalysisSession"]) == session_id()) {
       <div class="main-form">
         <form action="./authentication.php" method="POST">
           <div class="form-group">
-            <input type="email" class="form-control custom-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" />
+            <input type="email" class="form-control custom-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" required/>
           </div>
           <div class="form-group">
-            <input type="password" class="form-control custom-control" id="exampleInputPassword1" placeholder="Password" name="password" />
+            <input type="password" class="form-control custom-control" id="exampleInputPassword1" placeholder="Password" name="password" required/>
           </div>
           <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
             <label class="form-check-label" for="exampleCheck1">Remember me</label>
           </div>
           <button type="submit" class="btn btn-primary button-sub" name="loginSubmit">
